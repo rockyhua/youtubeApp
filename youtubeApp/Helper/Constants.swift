537ec01fac0927b5e27aac4c2ff42d3cbc455306
rockyhua <rockyhua123@gmail.com>
2020-09-21 15:11:37 +1000

    Constants.swift
    Please add your own API_KEY, PLAYLIST_ID, API_URL

---
+++ youtubeApp/Helper/Constants.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+struct Constants {
+  // Create your own API_list
+  static var API_KEY = " "
+  static var PLAYLIST_ID = " "
+  static var API_URL = " "
+  static var VIDEOCELL_ID = "VideoCell"
+
+  static var YT_EMBED_URL = "https://www.youtube.com/embed/"
+    
+  struct Storyboard {
+      static let homeViewController = "HomeVC"
+      static let homeNAV = "homeNAV"
+      static let loginVC = "LoginVC"
+  }
+}
